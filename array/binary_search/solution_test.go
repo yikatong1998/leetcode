775c04bbce56c0ee0520d467f4a9726d9599682a
yangxu <yangxu.third-party@outlook.com>
2022-09-13 23:48:57 +0800

    feat: solve binary-search

---
+++ array/binary_search/solution_test.go
@@ -0,0 +1,47 @@
+package binary_search
+
+import "testing"
+
+func Test_search(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test_1",
+			args: args{
+				nums:   []int{-1, 0, 3, 5, 9, 12},
+				target: 9,
+			},
+			want: 4,
+		},
+		{
+			name: "test_2",
+			args: args{
+				nums:   []int{-1, 0, 3, 5, 9, 12},
+				target: 2,
+			},
+			want: -1,
+		},
+		{
+			name: "test_3",
+			args: args{
+				nums:   []int{5},
+				target: 5,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := search(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("search() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
